ec334bae97cc9e725073f87c98ea4823756f13c8
ayoubbedraoui <ayoubbedraoui18@gmail.com>
2024-11-18 23:45:37 +0100

    model creation & data base configuration using mysql

---
+++ Back-end/src/main/java/tech/ayoub/employeemanager/model/Employee.java
@@ -0,0 +1,33 @@
+package tech.ayoub.employeemanager.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee implements Serializable {
+//    Serializable transform this class into different
+//     types of streams for example this class is
+//     gonna be registred in the DB and sent to the front as Json
+
+//    Map the class to a database as a table using JPA
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false,updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false,updatable = false)
+    private String employeeCode;
+
+
+}
